b6ee6881ca55a3fe3a9eef3ac7f51d2043767750
nagaharish12 <harishdb02@gmail.com>
2020-09-01 00:55:48 -0400

    typo error

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" /n
+            <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Fib Calculator version KUBERNETES</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
