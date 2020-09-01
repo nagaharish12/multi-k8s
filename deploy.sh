38942d2d807886fad97fe99d7ac26443c5cca060
nagaharish12 <harishdb02@gmail.com>
2020-09-01 00:04:19 -0400

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t harishnaga07/multi-client:latest -t harishnaga07/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t harishnaga07/multi-server:latest -t harishnaga07/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t harishnaga07/multi-worker:latest -t harishnaga07/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push harishnaga07/multi-client:latest
+docker push harishnaga07/multi-server:latest
+docker push harishnaga07/multi-worker:latest
+
+docker push harishnaga07/multi-client:$SHA
+docker push harishnaga07/multi-server:$SHA
+docker push harishnaga07/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=harishnaga07/multi-server:$SHA
+kubectl set image deployments/client-deployment client=harishnaga07/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=harishnaga07/multi-server:$SHA
+
